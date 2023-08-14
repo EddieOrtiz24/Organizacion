915f051f860046016f85f5db4fc37b04f092a396
Edgar Ortiz <edgar.ortizhdz@outlook.com>
2023-08-13 18:12:52 -0600

    Alpha2.1-Finalizado

---
+++ src/componentes/Header/header.jsx
@@ -0,0 +1,10 @@
+import "./header.css"
+
+function Header(){
+    return <header className="cabecera">
+
+        <img src="/img/headertotal.png" alt="Cabecera de la organización"/>
+        </header>
+}
+
+export default Header
